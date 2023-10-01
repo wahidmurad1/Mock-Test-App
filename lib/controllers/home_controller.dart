7940c729d2284e8a32be8a6e55d2b3de3e6e50de
wahidmurad1 <genie.murad@gmail.com>
2023-10-01 14:46:52 +0600

    start home page

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -2,7 +2,5 @@
 import 'package:mock_test_app/utils/constants/imports.dart';
 
 class HomeController extends GetxController{
-  final List sliderImageList = [
-    {'id': 1, 'image': 'assets/images/'}
-  ];
+  final RxList slider1ImageList = RxList();
 }
--- lib/utils/constants/resources.dart
+++ lib/utils/constants/resources.dart
@@ -5,3 +5,8 @@ List contactUsMessages = [
     'message': 'Here goes the descriptions of everything of contact us to Pricebond and other things. Here goes the descriptions of everything of contact us to Pricebond  and other things.'
   }
   ];
+  List sliderImageList = [
+    {'id': 1, 'image': 'assets/images/tip1.png'},
+    {'id': 1, 'image': 'assets/images/login.png'},
+    {'id': 1, 'image': 'assets/images/otp.png'},
+  ];
